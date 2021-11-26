ccf434aae7ad847a69829e2d73a593897f1de82f
Azmi Muhammad Syazwana <azmisyazwana@gmail.com>
2021-11-26 08:49:34 +0700

    add route resep and add-resep

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddResep from './components/AddResep';
 import Login from "./components/Login";
 import Register from './components/Register';
 import Home from './container/Home';
+import Resep from './container/Resep';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route exact path="/" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route  path="/home" element={<Home/>}/>
+        <Route  path="/resep" element={<Resep/>}/>
+        <Route  path="/add-resep" element={<AddResep/>}/>
       </Routes>
     </BrowserRouter>
   );
